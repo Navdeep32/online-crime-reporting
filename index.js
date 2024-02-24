fb68314029e5619e2abadda598ba0012524a7d28
Navdeep Kaur <navdeepkore283@gmail.com>
2024-02-24 10:52:46 +0530

    Database connectivity

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import nodemailer from "nodemailer";
 import crypto from "crypto";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import { OTP } from './otpModel.js';
+import { connectDB } from './db.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const otpMap = new Map();
-
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
-    host: "smtp.gmail.com",
+    host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
@@ -36,22 +38,27 @@ function generateOTP() {
     return crypto.randomBytes(3).toString('hex');
 }
 
-function sendOTP(email, otp) {
-    const hashedOTP = crypto.createHash('sha256').update(otp).digest('hex');
-    const mailOptions = {
-        from: 'crimereports3@gmail.com',
-        to: email,
-        subject: 'OTP Verification',
-        text: `Your OTP for verification is: ${otp}`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log('Error sending OTP:', error);
-        } else {
-            console.log('OTP sent:', info.response);
-        }
-    });
+async function sendOTP(email, otp) {
+    try {
+        const hashedOTP = crypto.createHash('sha256').update(otp).digest('hex');
+        await OTP.create({email, otp: hashedOTP});
+        const mailOptions = {
+            from: 'navdeepkore283@gmail.com',
+            to: email,
+            subject: 'OTP Verification',
+            text: `Your OTP for verification is: ${otp}. Please note that this OTP will expire after 5 minutes.`
+        };
+    
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log('Error sending OTP:', error);
+            } else {
+                console.log('OTP sent:', info.response);
+            }
+        });
+    } catch(error) {
+        console.error('Error sending OTP:', error);
+    }
 }
 
 app.get("/signup", (req, res) => {
@@ -63,7 +70,6 @@ app.post('/signup', (req, res) => {
     const otp = generateOTP();
     console.log("Generated otp: ", otp);
     sendOTP(email, otp);
-    otpMap.set(email, otp);
     res.redirect("/verify");
 });
 
@@ -76,8 +82,6 @@ app.post('/login', (req, res) => {
     const otp = generateOTP();
     console.log("Generated otp: ", otp);
     sendOTP(email, otp);
-    otpMap.set(email, otp);
-
     res.redirect("/verify");
 });
 
@@ -85,21 +89,32 @@ app.get("/verify", (req, res) => {
     res.sendFile(__dirname + "/verify.html");
 });
 
-app.post('/verify', (req, res) => {
-    const email = req.body.email;
-    const enteredOTP = req.body.otp1 + req.body.otp2 + req.body.otp3 + req.body.otp4 + req.body.otp5 + req.body.otp6;
+app.post('/verify', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const enteredOTP = req.body.otp1 + req.body.otp2 + req.body.otp3 + req.body.otp4 + req.body.otp5 + req.body.otp6;
 
-    console.log('Entered OTP:', enteredOTP);
-    console.log('Stored OTP:', otpMap.get(email));
+        const hashedEnteredOTP = crypto.createHash('sha256').update(enteredOTP).digest('hex');
+        const otpData = await OTP.findOne({ email, otp: hashedEnteredOTP });
 
-    if (!otpMap.has(email) || otpMap.get(email) !== enteredOTP) {
-        res.status(400).send('Invalid verfication code');
-    } else {
-        otpMap.delete(email);
-        res.status(200).send('Verified successfully');
+        if (!otpData) {
+            res.status(400).send('Invalid verification code');
+        } else {
+            await OTP.deleteOne({ email }); 
+            res.status(200).send('Verified successfully');
+        }
+    } catch (error) {
+        console.error('Error verifying OTP:', error);
+        res.status(500).send('Internal Server Error');
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running at port 3000");
+connectDB()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Server running at port 3000");
+        });
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
 });
